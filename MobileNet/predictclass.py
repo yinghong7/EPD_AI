8eca86b9f3675f296755d3fb258369dcdc7fa105
yinghong7 <29112302+yinghong7@users.noreply.github.com>
2024-05-07 09:31:26 +0800

    Update predictclass.py

--- MobileNet/predictclass.py
+++ MobileNet/predictclass.py
@@ -10,6 +10,8 @@ from keras.models import model_from_json
 from keras.models import Sequential, Model
 from keras.applications.mobilenet import preprocess_input, decode_predictions
 import pandas as pd
+import argparse
+
 model = tf.keras.applications.MobileNet(weights='imagenet')
 
 # ***Set your image folder & class list here***
@@ -18,8 +20,8 @@ model = tf.keras.applications.MobileNet(weights='imagenet')
 
 class mobileNet ():
     def __init__ (self):
-        self.folder = os.getcwd() + '\\Testimage'
-        with open (self.folder) as f:
+        self.folder = os.getcwd()
+        with open (self.folder + '\\allclasslist.txt') as f:
             self.classlist = f.readlines()
         self.allclass, self.output = [], []
         for lines in self.classlist:
@@ -27,19 +29,23 @@ class mobileNet ():
             self.text = self.text.strip("'")
             self.allclass.append (self.text)
     
-    def get_features (self):
-        for img in glob.glob(os.path.join(self.folder, '*')): #For each jpg in the imagefolder
-            img = keras.utils.load_img(img, target_size=(224, 224))
-            img = keras.utils.img_to_array(img)
-            img = np.expand_dims(img, axis=0)
-            img = preprocess_input(img)
-            item = np.array(model.predict(img)) # item is probability of our image being in each of the group of 1000 items in this model
-            whichmax = np.argmax(item)
-            output.append([whichmax, item[0][whichmax], self.allclass[whichmax]]) #[predicted ID, probability, predicited class name]
+    def get_features (self, location):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('--location', help='file_location')
+        args = parser.parse_args()
+        image = keras.utils.load_img(location, target_size=(224, 224))
+        img = keras.utils.img_to_array(image)
+        img = np.expand_dims(img, axis=0)
+        img = preprocess_input(img)
+        item = np.array(model.predict(img)) # item is probability of our image being in each of the group of 1000 items in this model
+        whichmax = np.argmax(item)
         print("%s seconds elapsed" % (time.time() - start_time))
     
 def main():
-    mobileNet().prediction()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--location', help='file_location')
+    args = parser.parse_args()
+    mobileNet().get_features(args.location)
 
 if __name__ == "__main__":
     main ()
